98345095b2e71b2401b57bbdd61ec17e57ca8ab5
Mona Lisa <mr.unknown2142@gmail.com>
2019-04-12 15:36:48 +0430

    test

--- inc/header.php
+++ inc/header.php
@@ -4,6 +4,9 @@
     <meta charset="utf-8">
     <title>وبوتل</title>
     <link rel="stylesheet" href="Style/style.css"><!--Main Style-->
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.4.0/dist/leaflet.css"
+          integrity="sha512-puBpdR0798OZvTTbP4A8Ix/l+A4dHDD0DGqYW6RQ+9jxkRFclaxxQb/SJAWZfWAkuyeQUytO7+7N4QKrDh+drA=="
+          crossorigin=""/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script><!--jQuery-->
     <script type="text/javascript" src="Scripts/main.js"></script>
